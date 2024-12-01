b684e5809a12d630c6c3640dff26d2af935c9482
Paola Novick <paola.novick@davinci.edu.ar>
2024-12-01 16:58:27 -0300

    abriendo api

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,36 @@
 /* eslint-disable react/prop-types */
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 
 const ItemList = ({ countries}) => {
   return (
-    <div>
-      {countries.map((countries, index) => (
-        <p key={index}>{countries.name}</p>
-      ))}
-    </div>
+   
+      <div>
+        {countries.map((country) => (
+          <Card key={country.cca3} style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+            <Card.Body>
+              <Card.Title>{country.name.common}</Card.Title>
+              <Card.Text>
+                <strong>Capital:</strong>{" "}
+                {country.capital ? country.capital[0] : "N/A"}
+                <br />
+                <strong>Region:</strong> {country.region}
+                <br />
+                <strong>Subregion:</strong> {country.subregion}
+                <br />
+              </Card.Text>
+              <Button variant="primary">Learn more</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+   
   );
 };
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import ItemList from "./ItemList"; // Asegúrate de que el componente ItemList esté importado correctamente
+
+
 
 const ItemListContainer = () => {
   const [countries, setCountries] = useState([]);
@@ -13,32 +14,7 @@ const ItemListContainer = () => {
       .then((res) => setCountries(res));
   }, []);
 
-  return (
-    <div>
-      {countries.map((country) => (
-        <Card key={country.cca3} style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src={country.flags[0]}
-            alt={`Flag of ${country.name.common}`}
-          />
-          <Card.Body>
-            <Card.Title>{country.name.common}</Card.Title>
-            <Card.Text>
-              <strong>Capital:</strong>{" "}
-              {country.capital ? country.capital[0] : "N/A"}
-              <br />
-              <strong>Region:</strong> {country.region}
-              <br />
-              <strong>Subregion:</strong> {country.subregion}
-              <br />
-            </Card.Text>
-            <Button variant="primary">Learn more</Button>
-          </Card.Body>
-        </Card>
-      ))}
-    </div>
-  );
+  return <ItemList countries={countries} />;
 };
 
 export default ItemListContainer;
